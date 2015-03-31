c042077ece13e437ade10deb76693477e5ea524c
oarchner <oliver.archner@uni-bayreuth.de>
2015-03-31 15:07:31 +0200

    Added distinct

--- grails-app/services/gateway/SplineService.groovy
+++ grails-app/services/gateway/SplineService.groovy
@@ -7,7 +7,7 @@ class SplineService {
 	
 	def getSplinesWithLockedFlag(Integer max, Integer offset) {		
 		def db = new Sql(dataSource)
-		def splines = db.rows("select s.id, s.name, c.id is not null locked from spline s left outer join channel c on c.spline_id = s.id order by s.name LIMIT ? OFFSET ?",[max, offset])
+		def splines = db.rows("select distinct s.id, s.name, c.id is not null locked from spline s left outer join channel c on c.spline_id = s.id order by s.name LIMIT ? OFFSET ?",[max, offset])
 		db.close()
 		return splines		
 	}
