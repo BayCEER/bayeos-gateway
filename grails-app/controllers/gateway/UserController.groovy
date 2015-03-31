7e35cffa6ece258974725f73d62fdbaddf64fad8
oarchner <oliver.archner@uni-bayreuth.de>
2015-03-31 15:12:22 +0200

    Print statement deleted

--- grails-app/controllers/gateway/UserController.groovy
+++ grails-app/controllers/gateway/UserController.groovy
@@ -43,9 +43,7 @@ class UserController {
 		}
     }
     
-    def edit() {
-		println(params)
-		
+    def edit() {			
 		def userInstance = User.get(params.id)
 		if (!userInstance) {
 			flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
