b0c8ba19fe7b8773eeca9eec3db18157e97e9d62
oarchner <oliver.archner@uni-bayreuth.de>
2015-06-05 11:11:05 +0200

    Added error and message type

--- test/integration/gateway/FrameSaveMessageTest.groovy
+++ test/integration/gateway/FrameSaveMessageTest.groovy
@@ -1,23 +1,36 @@
+import java.nio.ByteBuffer
+import java.util.prefs.Base64
 
-try {
-  host = "localhost"
-  user = "admin"
-  password = "xbee"  
-  url = new URL ("http://${host}/gateway/frame/saveFlat")	
-  con = url.openConnection()
-  con.setRequestMethod("POST")	
-  header = "Basic " + (user + ":" + password).bytes.encodeBase64().toString()
-  con.setRequestProperty("Authorization", header)	
-  data = "sender=TEST&bayeosframes[]=BEp1c3QgYSBtZXNzYWdlIQ"		
-  con.doOutput = true		
-  Writer wr = new OutputStreamWriter(con.outputStream)
-  wr.write(data)
-  wr.flush()
-  wr.close()		
-  con.connect()
-  println(con.content.text)
 
-} catch (Exception e) {
-	println(e.getMessage())
-	System.exit(3)
+host = "localhost:8080"
+user = "admin"
+password = "xbee"
+sender = "Foc1"
+message = "This is a test message"
+
+   
+for (type in [0x4, 0x5]){           
+      System.out.println("Sending message ${type}")
+      
+      url = new URL ("http://${host}/bayeos-gateway/frame/saveFlat")
+      con = url.openConnection()
+      con.setRequestMethod("POST")
+      header = "Basic " + (user + ":" + password).bytes.encodeBase64().toString()
+      con.setRequestProperty("Authorization", header)
+      con.doOutput = true
+
+      ByteBuffer bf = ByteBuffer.allocate(message.length() + 1)
+      bf.put((byte)type);
+      bf.put(message.getBytes());    
+      msg = Base64.byteArrayToBase64(bf.array());        
+            
+      data = "sender=${sender}&bayeosframes[]=${msg}"
+      Writer wr = new OutputStreamWriter(con.outputStream)
+      wr.write(data)
+      wr.flush()            
+      println(con.content.text)
+      wr.close()
 }
+  
+
+
