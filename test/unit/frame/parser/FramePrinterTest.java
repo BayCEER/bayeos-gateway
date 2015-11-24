7fb1b0c6bb91d5c3395f475a9738a40ab8a19fd8
oarchner <oliver.archner@uni-bayreuth.de>
2015-11-24 16:27:33 +0100

    Removed

--- test/unit/frame/parser/FramePrinterTest.java
+++ test/unit/frame/parser/FramePrinterTest.java
@@ -5,16 +5,15 @@ import java.io.IOException;
 import org.junit.Test;
 import org.springframework.security.core.codec.Base64;
 
-import frame.BayEOS;
-import frame.channel.DataFrame;
+import bayeos.frame.FrameConstants.NumberType;
+import bayeos.frame.data.*;
+
 
 public class FramePrinterTest {
 	
 	@Test
-	public void  printBase64() throws IOException {	
-				
-		System.out.println(new String(Base64.encode(new DataFrame<Integer>().addAll(1,2,3,4).getBytes(BayEOS.Number.Int32))));
-		
+	public void  printBase64() throws IOException {					
+		System.out.println(new String(Base64.encode(new DataFrame(NumberType.Int32,1,2,3,4).getBytes())));		
 	}
 
 }
