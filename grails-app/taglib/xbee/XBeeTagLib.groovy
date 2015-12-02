79b2684d515fc985e120980125a99bfd954359e6
oarchner <oliver.archner@uni-bayreuth.de>
2015-12-02 13:37:53 +0100

    Fixed image path

--- grails-app/taglib/xbee/XBeeTagLib.groovy
+++ grails-app/taglib/xbee/XBeeTagLib.groovy
@@ -9,15 +9,15 @@ class XBeeTagLib {
 		if (attrs.rssi != null){
 			def rssi = attrs.rssi.toInteger()
 			if (rssi < -85){
-				out << "<img src=\"${resource(dir:'images/rssi',file:'c1.gif')}\"/>"
+				out << "<img src=\"${resource(dir:'images',file:'level_1.gif')}\"/>"
 			} else if (rssi >= -85 && rssi < -75){
-				out << "<img src=\"${resource(dir:'images/rssi',file:'c2.gif')}\"/>"
+				out << "<img src=\"${resource(dir:'images',file:'level_2.gif')}\"/>"
 			} else if (rssi >= -75 && rssi < -65){
-				out << "<img src=\"${resource(dir:'images/rssi',file:'c3.gif')}\"/>"
+				out << "<img src=\"${resource(dir:'images',file:'level_3.gif')}\"/>"
 			} else if (rssi >= -65 && rssi < -45){
-				out << "<img src=\"${resource(dir:'images/rssi',file:'c4.gif')}\"/>"
+				out << "<img src=\"${resource(dir:'images',file:'level_4.gif')}\"/>"
 			} else if (rssi >= -45){
-				out << "<img src=\"${resource(dir:'images/rssi',file:'c5.gif')}\"/>"
+				out << "<img src=\"${resource(dir:'images',file:'level_5.gif')}\"/>"
 			}
 		}
 	}
