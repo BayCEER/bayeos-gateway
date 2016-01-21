20f4750fe5957f6885dfec360d2da4079507d8d8
oarchner <oliver.archner@uni-bayreuth.de>
2016-01-21 14:44:16 +0100

    Revised NaN integration test

--- test/integration/gateway/FrameServiceTests.groovy
+++ test/integration/gateway/FrameServiceTests.groovy
@@ -10,6 +10,7 @@ import bayeos.frame.data.*;
 import bayeos.frame.wrapped.*;
 import gateway.FrameService
 import gateway.Board
+import groovy.sql.Sql
 
 
 
@@ -17,6 +18,9 @@ class FrameServiceTests {
 	static transactional = false
 	
 	def frameService	
+	def observationService
+	def dataSource
+	
 	
     @Before
     void setUp() {									 		
@@ -38,19 +42,46 @@ class FrameServiceTests {
 		}
 		frameService.saveFrames("testSaveFlat",frames)		
 		Board b = Board.findByOrigin("testSaveFlat")
-		assert(b)				
-		b.delete();						
+		
+		Sql db = new Sql(dataSource)		
+		def row = db.firstRow("select count(*) from observation, board, channel where observation.channel_id = channel.id and channel.board_id = board.id and board.id = ?",[b.id])
+		assert 40000 == row[0]
+		db.close()		
+		b.delete(flush:true);						
     }
 	
 	
 	@Test
 	void testMessageFrame() {
-		MsgFrame msg = new MsgFrame("This is a message")		
+		MsgFrame msg = new MsgFrame("This is a test message")		
 		frameService.saveFrames("testMessageFrame",new String(Base64.encodeBase64String(msg.getBytes())));
-		Board b = Board.findByOrigin("testMessageFrame")
-		b.delete();
+		Board b = Board.findByOrigin("testMessageFrame")		
+		Message m = Message.findByOrigin(b.getOrigin())
+		assert "This is a test message" == m.getContent()		
+		b.delete(flush:true);
+		m.delete(flush:true);
 				
 	}
+	
+	@Test
+	void testNaN() {
+		
+		Date now = new Date()		
+		MillisecondTimestampFrame frame = new MillisecondTimestampFrame(now, new DataFrame(NumberType.Float32, Float.NaN,1.0F).getBytes())
+		frameService.saveFrames("testNaN",new String(Base64.encodeBase64String(frame.getBytes())))
+		Board b = Board.findByOrigin("testNaN")
+				
+		def data = observationService.findByIdAndRowId(b.id.toInteger(),null)		
+		
+		assert 1 == data.size()		
+		assert 2 == data[0]['channel_nr']
+		assert now.getTime() == data[0]['result_time'].getTime()
+		assert 1.0F == data[0]['result_value']
+		
+		b.delete(flush:true);
+		
+	}
+	
 	
 //	@Test
 //	void testPosts() {
