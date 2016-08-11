d8133b8df86e76f3ecdcc372bcd9e805041de863
oliver archner <oliver.archner@uni-bayreuth.de>
2016-08-11 12:44:06 +0200

    Fixed wrong encoding for messages.

--- grails-app/controllers/gateway/NagiosController.groovy
+++ grails-app/controllers/gateway/NagiosController.groovy
@@ -1,35 +1,36 @@
 package gateway
 
-import grails.converters.JSON
 import grails.converters.XML
+import grails.converters.JSON
 
 class NagiosController {
 	
 	def nagiosService
-	
-		
+				
 	// Example call: curl -v -i -u nagios:xbee localhost:8080/bayeos-gateway/nagios/msgGateway
-	def msgGateway(){
-		def msg = nagiosService.msgGateway();
+	def msgGateway(){							
+		def msg = nagiosService.msgGateway()
 		withFormat {
-			xml { render msg as XML}
-			json { render msg as JSON}
+			xml{render(text:msg as XML,contentType:"text/xml",encoding:"UTF-8")}
+			json{render(text:msg as JSON,contentType:"text/json",encoding:"UTF-8")}			
 		}
+				
 	}
 	
 	def msgExporter(){
 		def msg = nagiosService.msgExporter();
 		withFormat {
-			xml { render msg as XML}
-			json { render msg as JSON}
+			xml{render(text:msg as XML,contentType:"text/xml",encoding:"UTF-8")}
+			json{render(text:msg as JSON,contentType:"text/json",encoding:"UTF-8")}
 		}
+		
 	}
 
 	def msgGroup(){
 		def msg = nagiosService.msgGroup(params.name)
 		withFormat {
-			xml { render msg as XML}
-			json { render msg as JSON}
+			xml{render(text:msg as XML,contentType:"text/xml",encoding:"UTF-8")}
+			json{render(text:msg as JSON,contentType:"text/json",encoding:"UTF-8")}
 		}
 	}
 	
@@ -37,16 +38,16 @@ class NagiosController {
     def msgBoard() { 
 		def msg = nagiosService.msgBoard(params.int('id'))
 		withFormat {
-			xml { render msg as XML}
-			json { render msg as JSON}
+			xml{render(text:msg as XML,contentType:"text/xml",encoding:"UTF-8")}
+			json{render(text:msg as JSON,contentType:"text/json",encoding:"UTF-8")}
 		}
 	}
 	
 	def msgChannel(){
 		def msg = nagiosService.msgChannel(params.int('id'))
 		withFormat {
-			xml { render msg as XML}
-			json { render msg as JSON}
+			xml{render(text:msg as XML,contentType:"text/xml",encoding:"UTF-8")}
+			json{render(text:msg as JSON,contentType:"text/json",encoding:"UTF-8")}
 		}
 	}
 	
