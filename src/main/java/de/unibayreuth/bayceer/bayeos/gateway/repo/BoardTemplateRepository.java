db95cc758ccc945a5724d04d5a6e2d3045858ab7
oliver archner <oliver.archner@uni-bayreuth.de>
2017-01-12 12:23:32 +0100

    Changed import

--- src/main/java/de/unibayreuth/bayceer/bayeos/gateway/repo/BoardTemplateRepository.java
+++ src/main/java/de/unibayreuth/bayceer/bayeos/gateway/repo/BoardTemplateRepository.java
@@ -2,7 +2,6 @@ package de.unibayreuth.bayceer.bayeos.gateway.repo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import de.unibayreuth.bayceer.bayeos.gateway.model.BoardGroup;
 import de.unibayreuth.bayceer.bayeos.gateway.model.BoardTemplate;
 
 public interface BoardTemplateRepository extends PagingAndSortingRepository<BoardTemplate, Long>{
