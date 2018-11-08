6d966444e3e1862b5b1641831ae41aa11b7f3d06
oliver archner <oliver.archner@uni-bayreuth.de>
2018-11-08 13:58:25 +0100

    Query vc by board id

--- src/main/java/de/unibayreuth/bayceer/bayeos/gateway/repo/VirtualChannelRepository.java
+++ src/main/java/de/unibayreuth/bayceer/bayeos/gateway/repo/VirtualChannelRepository.java
@@ -1,5 +1,7 @@
 package de.unibayreuth.bayceer.bayeos.gateway.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import de.unibayreuth.bayceer.bayeos.gateway.model.VirtualChannel;
@@ -7,5 +9,6 @@ import de.unibayreuth.bayceer.bayeos.gateway.model.VirtualChannel;
 public interface VirtualChannelRepository extends PagingAndSortingRepository<VirtualChannel, Long> {
 		
 	VirtualChannel findFirstByOrderByNrDesc();
+	List<VirtualChannel> findByBoardId(Long id);
 	
 }
