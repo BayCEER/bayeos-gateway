c5765547a1b4ee80a34b89382511afcfad19c43e
oliver archner <oliver.archner@uni-bayreuth.de>
2018-11-22 15:18:04 +0100

    Anonymous bind in search

--- src/main/java/de/unibayreuth/bayceer/bayeos/gateway/ldap/LdapRestController.java
+++ src/main/java/de/unibayreuth/bayceer/bayeos/gateway/ldap/LdapRestController.java
@@ -1,6 +1,5 @@
 package de.unibayreuth.bayceer.bayeos.gateway.ldap;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -8,7 +7,6 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,72 +21,84 @@ import com.novell.ldap.LDAPJSSESecureSocketFactory;
 import com.novell.ldap.LDAPSearchResults;
 
 @RestController
-public class LdapRestController {		
-	
+public class LdapRestController {
+
 	@Value("${LDAP_BASE:ou=users}")
-	private String ldap_base;	
-		
-	@Autowired
-	LdapAuthenticationProvider authProv;
-	
+	private String ldap_base;
+
+	@Value("${LDAP_HOST:localhost}")
+	private String ldap_host;
+
+	@Value("${LDAP_PORT:636}")
+	private int ldap_port;
+
+	@Value("${LDAP_SSL:true}")
+	private Boolean ldap_ssl;
+
+	@Value("${LDAP_VERSION:3}")
+	private int ldap_version;
+
+	@Value("${LDAP_SN:sn}")
+	private String ldap_sn;
+
+	@Value("${LDAP_GIVEN_NAME:givenName}")
+	private String ldap_givenName;
+
+	@Value("${LDAP_SEARCH:false}")
+	public boolean ldap_search;
+
 	private Logger log = LoggerFactory.getLogger(LdapRestController.class);
-		    	
-	@RequestMapping(path="/rest/ldap/search", method = RequestMethod.GET)
-	public List<Map<String,String>> search(String filter){
-		return search(filter,new String[] {"cn", authProv.getGivenName(),authProv.getSn()});		
+
+	@RequestMapping(path = "/rest/ldap/search", method = RequestMethod.GET)
+	public List<Map<String, String>> search(String filter) {
+		return search(filter, new String[] { "cn", ldap_givenName, ldap_sn });
 	}
-	
-	@RequestMapping(path="/rest/ldap/searchUser", method = RequestMethod.GET)
-	public List<Map<String,String>> searchUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String userName) {		
-		return search(String.format("(&(%s=%s)(%s=%s)(%s=%s))","cn",userName,authProv.getGivenName(),firstName,authProv.getSn(),lastName));		
+
+	@RequestMapping(path = "/rest/ldap/searchUser", method = RequestMethod.GET)
+	public List<Map<String, String>> searchUser(@RequestParam String firstName, @RequestParam String lastName,
+			@RequestParam String userName) {
+		return search(String.format("(&(%s=%s)(%s=%s)(%s=%s))", "cn", userName, ldap_givenName, firstName, ldap_sn,
+				lastName));
 	}
-	
-	private List<Map<String,String>> search(String filter, String[] attributes){
-		log.debug("Filter:" + filter);
-		if (authProv != null) {
-			LDAPConnection con = null;		
-			try {							
-				if (authProv.getSsl()) {
+
+	private List<Map<String, String>> search(String filter, String[] attributes) {
+		if (ldap_search) {
+			log.debug("Filter:" + filter);
+			LDAPConnection con = null;
+			try {
+				if (ldap_ssl) {
 					con = new LDAPConnection(new LDAPJSSESecureSocketFactory());
 				} else {
 					con = new LDAPConnection();
-				}										
-				con.connect(authProv.getHost(), authProv.getPort());
-				String dname  = String.format(authProv.getDn(),authProv.getUser().getName());
-				con.bind(authProv.getVersion(),dname, authProv.getUser().getPassword().getBytes("UTF-8"));			
-				if (con.isBound()) {								
-					LDAPSearchResults res =  con.search(ldap_base,LDAPConnection.SCOPE_SUB , filter, attributes, false);
-					List<Map<String,String>> entries = new ArrayList<>(res.getCount());					
-					while(res.hasMore()) {
-						LDAPEntry e = res.next();
-						Map<String,String> a = new Hashtable<>(attributes.length);																		
-						for(String key:attributes) {
-							LDAPAttribute at = e.getAttribute(key);
-							if (at!=null) {
-								a.put(key, at.getStringValue());	
-							}							
+				}
+				con.connect(ldap_host, ldap_port);
+				LDAPSearchResults res = con.search(ldap_base, LDAPConnection.SCOPE_SUB, filter, attributes, false);
+				List<Map<String, String>> entries = new ArrayList<>(res.getCount());
+				while (res.hasMore()) {
+					LDAPEntry e = res.next();
+					Map<String, String> a = new Hashtable<>(attributes.length);
+					for (String key : attributes) {
+						LDAPAttribute at = e.getAttribute(key);
+						if (at != null) {
+							a.put(key, at.getStringValue());
 						}
-						entries.add(a);
-					}					
-					log.debug(entries.size() + " entries found");
-					return entries;
-				}				
-			} catch (LDAPException | UnsupportedEncodingException e) {
-				log.error(e.getMessage());				
+					}
+					entries.add(a);
+				}
+				log.debug(entries.size() + " entries found");
+				return entries;
+			} catch (LDAPException e) {
+				log.error(e.getMessage());
 			} finally {
 				try {
 					if (con != null && con.isConnected()) {
-						con.disconnect();	
-					}				
+						con.disconnect();
+					}
 				} catch (LDAPException e) {
 					log.error(e.getMessage());
 				}
-			}				
-		}					
+			}
+		}
 		return null;
 	}
-	
-	
-		
-	
 }
