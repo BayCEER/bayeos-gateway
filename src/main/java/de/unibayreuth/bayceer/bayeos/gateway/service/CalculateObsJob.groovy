677c5302250088a49eed32b3f714a142a8b3294e
oliver archner <oliver.archner@uni-bayreuth.de>
2020-09-07 09:20:21 +0200

    Merge 2.6.4

--- src/main/java/de/unibayreuth/bayceer/bayeos/gateway/service/CalculateObsJob.groovy
+++ src/main/java/de/unibayreuth/bayceer/bayeos/gateway/service/CalculateObsJob.groovy
@@ -15,20 +15,19 @@ import bayeos.frame.types.LabeledFrame
 
 
 @Component
-@Profile("default")
 public class CalculateObsJob implements Runnable {
-	
+
 	@Autowired
-	private DataSource dataSource	
-	
+	private DataSource dataSource
+
 	@Autowired
 	private FrameService frameService
-		
+
 	@Value('${CALC_WAIT_SECS:120}')
 	private int waitSecs
-	
+
 	private Logger log = Logger.getLogger(CalculateObsJob.class)
-	
+
 	@PostConstruct
 	public void start(){
 		new Thread(this).start()
@@ -37,41 +36,68 @@ public class CalculateObsJob implements Runnable {
 	@Override
 	public void run() {
 		Thread.sleep(1000*waitSecs);
-		while(true) {				
-		 def exit = -1
-		 def start = new Date()
-		 def rowCalculated = 0
-		 def rowArchived = 0
-		 
-		 try {		
-			def db = new Sql(dataSource)
-			try {		
-				log.info("CalculateJob running")			
-				def ts = new Date().toTimestamp()
-				def id = db.firstRow("select max(id) from observation")
-				if (id == null){
-					log.info("Nothing to calculate");										
-				} else {					
-					rowCalculated = db.executeUpdate('insert into observation_calc (id, channel_id, result_time, result_value) select id, channel_id, result_time, result_value from get_bayeos_obs(?)',[ts])
-					log.info("${rowCalculated} observations calculated")					
-					log.info("Move records to archive table.")
-					rowArchived = db.call("{call delete_obs(?,?)}",[ts,id.max])										
+		while(true) {
+			def exit = -1
+			def start = new Date()
+			def rowCalculated = 0
+			def rowArchived  = 0
+			try {
+				def db = new Sql(dataSource)
+				try {
+					log.info("CalculateJob running")
+					def ts = new Date().toTimestamp()
+					def r = db.firstRow("select max(id) as id from observation")
+					if (r == null){
+						log.info("Nothing to calculate");
+					} else {
+						def id = r.id
+
+
+						db.eachRow("""SELECT c.id, c.db_series_id, c.spline_id, f.name AS f_name, i.name AS i_name, c.filter_critical_values,c.critical_min,c.critical_max
+									FROM channel c 
+									LEFT JOIN function f ON f.id = c.aggr_function_id 
+									LEFT JOIN "interval" i ON i.id = c.aggr_interval_id 
+									JOIN board b on b.id = c.board_id 
+									where b.db_auto_export and b.db_folder_id is not null 
+									and c.db_series_id is not null and c.db_exclude_auto_export = false"""){ cha ->
+
+									// Fill table for ExportJopb
+									rowCalculated = rowCalculated + db.executeUpdate("""
+										insert into observation_calc (channel_id, result_time, result_value)
+										select ${cha.id},c.result_time, c.result_value from 
+										get_channel_time_value_flag(${ts},${cha.id},${cha.spline_id},${cha.f_name},${cha.i_name},
+										${cha.filter_critical_values},${cha.critical_min}, ${cha.critical_max}) as c where c.flag < 1
+										""");
+
+									// Move observations to archive table
+									if (cha.i_name == null) {
+										db.execute("""insert into observation_exp select * from observation where id<=${id} and channel_id = ${cha.id} and result_time<${ts}""")
+										db.execute("""delete from observation where id<=${id} and channel_id=${cha.id} and result_time<${ts}""")
+
+									} else {
+										db.execute("""insert into observation_exp select * from observation where id<=${id} and channel_id = ${cha.id} and result_time<date_truncate(${ts}, ${cha.i_name}::interval);""")
+										db.execute("""delete from observation where id<=${id} and channel_id=${cha.id} and result_time<date_truncate(${ts}, ${cha.i_name}::interval);""")
+									}
+									rowArchived = rowArchived + db.updateCount
+								}
+								log.info("${rowCalculated} records calculated.")
+
+					}
+					exit = 0
+				} catch (SQLException e){
+					log.error(e.getMessage())
+					exit = -1
+				}  finally {
+					db.close()
+					log.info("CalculateJob finished")
 				}
-				exit = 0
-			} catch (SQLException e){
-				log.error(e.getMessage())
-				exit = -1	
-			}  finally {
-				db.close()
-				log.info("CalculateJob finished")
+				def millis = (new Date()).getTime() - start.getTime()
+				frameService.saveFrame("\$SYS/CalculateJob",new LabeledFrame(NumberType.Float32,"{'exit':${exit},'calculated':${rowCalculated},'archived':${rowArchived},'millis':${millis}}".toString()))
+				Thread.sleep(1000*waitSecs);
+			} catch (InterruptedException e){
+				break;
 			}
-			def millis = (new Date()).getTime() - start.getTime()			
-			frameService.saveFrame("\$SYS/CalculateJob",new LabeledFrame(NumberType.Float32,"{'exit':${exit},'calculated':${rowCalculated},'archived':${rowArchived},'millis':${millis}}".toString()))
-			Thread.sleep(1000*waitSecs);
-		 } catch (InterruptedException e){
-			 break;
-		 }
-		}					
+		}
 	}
 
 }
