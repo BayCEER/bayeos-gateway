d89efa42f61a4a48aa4711179e8ba64da7c5aaf3
oliver archner <oliver.archner@uni-bayreuth.de>
2022-06-20 08:31:24 +0200

    Sin channel to host debian

--- tools/FramePump.py
+++ tools/FramePump.py
@@ -4,26 +4,30 @@ from bayeosgatewayclient import BayEOSWriter, BayEOSSender
 import logging
 import tempfile
 from os import path
+import math 
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s:%(message)s")                
 
 NAME = 'FramePump'
-URL = 'http://localhost:5533/frame/saveFlat'
+URL = 'http://debian/gateway/frame/saveFlat'
 USER= 'root'
 PASSWORD = 'bayeos'
 PATH = path.join(tempfile.gettempdir(),'bayeos-device')
 BACKUP_PATH =  path.join(tempfile.gettempdir(),'bayeos-device-backup')
 
-writer = BayEOSWriter(PATH,max_time=10,log_level=logging.DEBUG)
+writer = BayEOSWriter(PATH,max_time=5,log_level=logging.INFO)
 writer.save_msg('Writer was started.')
 
-sender = BayEOSSender(PATH, NAME, URL,backup_path=BACKUP_PATH,log_level=logging.DEBUG,user=USER,password=PASSWORD)
+sender = BayEOSSender(PATH, NAME, URL,backup_path=BACKUP_PATH,log_level=logging.INFO,user=USER,password=PASSWORD)
 sender.start()
 
 nr=0
+angle = 0
 while True:
-    writer.save([nr, 3, 20.5])
-    writer.flush()
+    writer.save([nr,angle ,10*math.sin(angle)])    
     nr+=1
-    sleep(1)
+    angle+=0.1
+    if angle > 360:
+        angle = 0
+    sleep(0.5)
