d6c0d770713ae81b9badd7bb8dfb5d893987e6af
oliver archner <oliver.archner@uni-bayreuth.de>
2022-09-21 10:08:09 +0200

    Deprecated api replaced

--- src/main/java/de/unibayreuth/bayceer/bayeos/gateway/websocket/WebSocketConfig.java
+++ src/main/java/de/unibayreuth/bayceer/bayeos/gateway/websocket/WebSocketConfig.java
@@ -1,13 +1,14 @@
 package de.unibayreuth.bayceer.bayeos.gateway.websocket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
 	
 	
     @Override
