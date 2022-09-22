b30945640b20b2743a544152b3716ee19c7ef77a
oliver archner <oliver.archner@uni-bayreuth.de>
2022-09-22 09:24:44 +0200

    ws allowed origin pattern added

--- src/main/java/de/unibayreuth/bayceer/bayeos/gateway/websocket/WebSocketConfig.java
+++ src/main/java/de/unibayreuth/bayceer/bayeos/gateway/websocket/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
